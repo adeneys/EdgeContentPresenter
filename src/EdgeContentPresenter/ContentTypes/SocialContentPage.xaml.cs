bdf16fead830ebdb1a2c057efb996dc912be30ac
Alistair Deneys <alistair.deneys@gmail.com>
2023-08-23 18:14:48 +0530

    Make placement of text on social page relative to page size

--- src/EdgeContentPresenter/ContentTypes/SocialContentPage.xaml.cs
+++ src/EdgeContentPresenter/ContentTypes/SocialContentPage.xaml.cs
@@ -6,4 +6,12 @@ public partial class SocialContentPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+        Labels.TranslationX = width * 0.22;
+        Labels.TranslationY = height * 0.61;
+        Labels.Spacing = width * 0.08;
+    }
 }
